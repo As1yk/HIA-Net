c4fa1e9f4dbddc0180ce9c3c35416c20acae7989
As1yk <3503469001@qq.com>
2024-11-15 20:04:15 +0800

    code

--- model.py
+++ model.py
@@ -7,11 +7,11 @@ class MyModel(nn.Module):
         super(MyModel, self).__init__()
         self.EEGnet = ResCBAM()
         self.ETnet = DenseNet1D()
-        self.encoder = DIA(eeg_input_dim, eye_input_dim , d_model=output_dim)
+        self.DIA = DIA(eeg_input_dim, eye_input_dim , d_model=output_dim)
 
     def forward(self, eeg_input, eye_input):
         eeg_features = self.EEGnet(eeg_input)
         et_features = self.ETnet(eye_input)
-        fusion = self.encoder(eeg_features, et_features)
+        fusion = self.DIA(eeg_features, et_features)
         return fusion
 
